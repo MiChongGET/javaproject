697ae42f9b687a90fc35796a546640228593c806
MiChong <1564666023@qq.com>
2016-09-17 23:34:56 +0800

    最终版

--- src/com/michong/Tianqi.java
+++ src/com/michong/Tianqi.java
@@ -8,10 +8,18 @@ public class Tianqi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String capter="牡丹江:101050301佳木斯:101050401绥化:101050501黑河:101050601双鸭山:101051301";
+		String capter="北京:101010100朝阳:101010300顺义:101010400怀柔:101010500通州:101010600昌平:101010700延庆:101010800丰台:101010900石景山:101011000大兴:101011100房山:101011200密云:101011300门头沟:101011400平谷:101011500八达岭:101011600佛爷顶:101011700";
 		System.out.println("总长度："+capter.length());
-		int[] num =new int[5];
 		char s=':';
+		int d=0;
+		for(int i=0;i<capter.length();i++){
+			if(capter.charAt(i)==s){
+				d++;
+			}	
+		}
+		
+		int[] num =new int[d];
+		
 		int m=0;
 		for(int i=0;i<capter.length();i++){
 			if(capter.charAt(i)==s){
@@ -29,9 +37,14 @@ public class Tianqi {
 		
 		int b=2;
 		for(int a=0;a<m-1;a++){
-			System.out.println("b为："+b);
+			//System.out.println("b为："+b);
 			b=num[a+1]-num[a]-10;
-			System.out.println(capter.substring(num[a]-b,num[a]+10)+"\n");
+			//第一个城市
+			if(a==0){
+		     System.out.println(capter.substring(num[a]-b,num[a]+10)+"\n");
+			}
+			//剩下的城市
+			System.out.println(capter.substring(num[a+1]-b,num[a+1]+10)+"\n");
 		
 	}
 	}
